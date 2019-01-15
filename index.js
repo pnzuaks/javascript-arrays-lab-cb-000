f50027bc6c11db9ee65932b17ee03f5b73586574
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-15 23:36:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"]//define your array here
 
 function destructivelyAppendKitten(name) {
-
+  kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name) {
