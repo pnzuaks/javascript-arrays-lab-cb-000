cf97e66272f2cc60fadc4f47dc607c38a439a6ee
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-15 23:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name) {
   kittens.push(name)
   return kittens
 }
-function appendKitten(name) {
+function removeLastKitten(name) {
   kittens.push(name)
   return kittens
 }
